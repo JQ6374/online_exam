8f95a5915cd743baebdb5e4302c1f20c30645df4
Zzq74 <2410360091@qq.com>
2023-05-16 14:35:25 +0800

    注册、整合-2

--- exam-springboot/src/main/java/com/zz/bean/Exam.java
+++ exam-springboot/src/main/java/com/zz/bean/Exam.java
@@ -11,5 +11,5 @@ public class Exam {
     private String name;
     private LocalDateTime start_time;
     private LocalDateTime end_time;
-    private String status;
+    private Integer status;
 }
